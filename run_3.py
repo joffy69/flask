22311b059791c81e89d73cbfefa3a4a69d21a664
joffy69 <jcwgibbs@gmail.com>
2020-10-15 20:11:00 +0000

    First commit by me
    not functional a ce moment - hopefully restart will work

---
+++ run_3.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/abooot")
+def abooot():
+    return render_template("abooot.html")
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+         port=int(os.environ.get("PORT")),
+         debug=True)
